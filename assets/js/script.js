db930ca525e64be52a3f819acb2ab44a39505f9f
Daniel Conlon <danielcconlon@gmail.com>
2022-08-16 21:01:30 -0400

    pwa made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 require("bootstrap");
 
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
 $(document).ready(function() {
 
